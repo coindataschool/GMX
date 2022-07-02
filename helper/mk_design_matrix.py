8b46fc06166f126c6f3d643a2b351a3124414b10
Coin Data School <coindataschool@gmail.com>
2022-07-02 16:44:21 +0900

    add function for creating design matrix of with polynomial terms and default seasonality (if date index has daily frequency, the default seasonality features would be Monday to Sunday)

--- helper/mk_design_matrix.py
+++ helper/mk_design_matrix.py
@@ -1,7 +1,8 @@
 from statsmodels.tsa.deterministic import DeterministicProcess
+import pandas as pd
 
 def mk_X_linear_trend(dates):
-    """Create design matrix based off dates to be used for fitting a linear regression.
+    """Create design matrix for fitting a linear regression.
     
     Arguments:
     dates -- an index or a list of 'datetime64[ns]' objects
@@ -13,3 +14,28 @@ def mk_X_linear_trend(dates):
         drop=True
     )
     return dp.in_sample()
+
+
+def mk_X_trend_seasonality(dates, order=1, include_default_seasonality=True):
+    """Create design matrix for fitting a linear regression.
+    
+    Arguments:
+    dates -- an index or a list of 'datetime64[ns]' objects
+    """
+    dp = DeterministicProcess(
+        index=dates,
+        constant=False, # cuz scikit-learn linear regression fits the intercept by default
+        order=order,
+        seasonal=include_default_seasonality,
+        drop=True
+    )
+    return dp.in_sample()
+
+
+def make_lags(ts, lags, lead_time=1, name='y'):
+    return pd.concat(
+        {
+            f'{name}_lag_{i}': ts.shift(i)
+            for i in range(lead_time, lags + lead_time)
+        },
+        axis=1)
