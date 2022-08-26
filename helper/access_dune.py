f64fdf0d32362ced83a9bed812be5dcad900b60b
Coin Data School <coindataschool@gmail.com>
2022-08-26 10:24:32 +0900

    sort data obtained from dune by date in ascending order; this is a double ensurance

--- helper/access_dune.py
+++ helper/access_dune.py
@@ -10,7 +10,7 @@ def extract_frame_from_dune_data(dune_data, date_col='day'):
     df['date'] = pd.to_datetime(df[date_col].str.replace('T.*', '', regex=True))
     if date_col != 'date':
         df = df.drop(date_col, axis=1)
-    df = df.set_index('date')
+    df = df.set_index('date').sort_index()
     # drop the last row cuz it may not always be a full day
     return df.iloc[:-1, :]
 
